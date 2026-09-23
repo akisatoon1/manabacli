9ad02fb6b5307b3e71c53179f7a5dc2be6384350
akisatoon <akisatoon@gmail.com>
2026-09-23 20:22:30 +0900

    fix: root コマンドの説明を修正

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -9,12 +9,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "manabacli",
 	Short: "manaba に関する処理を行う CLI ツール",
-	Long: `使い方: manabacli <URL> <ファイル名>...
-
-  <URL>         提出先の manaba ページ URL
-  <ファイル名>  manaba にアップロードするファイルのパス（複数指定可）
-
-認証情報は ~/.manabacli/config に以下の形式で保存してください:
+	Long: `認証情報は ~/.manabacli/config に以下の形式で保存してください:
   username=ユーザー名
   password=パスワード`,
 }
